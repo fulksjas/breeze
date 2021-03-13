ae8ba7c146eea38f87578eca0d1e704f8300f6cc
Fulks, Jason (CORP) <jason.fulks@adp.com>
2021-03-12 20:17:06 -0700

    the mat table, sort, and pagination is working.  Updated landing module with import and added

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 
 import { Contact } from 'src/app/model/contact.model';
 import { ContactsService } from 'src/app/services/contacts.service';
@@ -12,20 +12,18 @@ import { ContactsService } from 'src/app/services/contacts.service';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit, AfterViewInit {
-  @ViewChild(MatPaginator)
-  paginator: MatPaginator;
-  @ViewChild(MatSort)
-  sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  dataSource = new MatTableDataSource<Contact>();
-  columnsToDisplay = ['id', 'firstName', 'lastName'];
+  contactData = new MatTableDataSource<Contact>();
+  contactColumns = ['firstName', 'lastName', 'address', 'email', 'phone'];
 
   constructor(private contactsService: ContactsService) {
   }
 
   ngOnInit() {
     this.contactsService.getContactsSource().subscribe((contacts: Contact[]) => {
-      this.dataSource = new MatTableDataSource<Contact>(contacts);
+      this.contactData = new MatTableDataSource<Contact>(contacts);
     });
 
     this.contactsService.loadContacts();
@@ -33,7 +31,7 @@ export class LandingComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     // add ngAfterViewInit hook
-       this.dataSource.paginator = this.paginator;
-       this.dataSource.sort = this.sort;
+       this.contactData.paginator = this.paginator;
+       this.contactData.sort = this.sort;
    }
 }
--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -2,11 +2,17 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing.component';
 import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   imports: [
     CommonModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule
   ],
   declarations: [LandingComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
